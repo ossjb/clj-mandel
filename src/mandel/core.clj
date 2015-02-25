f3167e8b924bebd0cfc5ed3b32ef2474c0ff45a1
Johannes Beigel <johannes@beigel.de>
2015-02-25 10:55:57 +0100

    calc mandelbrot set

--- src/mandel/core.clj
+++ src/mandel/core.clj
@@ -4,7 +4,19 @@
            (javax.swing JFrame))
   (:gen-class))
 
-(set! *warn-on-reflection* true)
+;(set! *warn-on-reflection* true)
+
+(defn mandelbrot
+  [^double cx ^double cy ^long max-iterations]
+  (loop [x 0
+         y 0
+         iter 0]
+    (let [abs-squared (+ (* x x) (* y y))]
+      (if (or (> abs-squared 8) (= iter max-iterations))
+        (- iter (/ (Math/log (/ (Math/log abs-squared) (Math/log 4))) (Math/log 2)))
+        (recur (+ (- (* x x) (* y y)) cx)
+               (+ (* 2 x y) cy)
+               (unchecked-inc iter))))))
 
 (defn hue-to-rgb ^long
   [^long hue]
@@ -19,22 +31,28 @@
       (< hue 360) (unchecked-add-int 0xff0000 x))))
 
 (defn get-color ^long
-  [^long x ^long y ^long width]
-  (hue-to-rgb (unchecked-divide-int (unchecked-multiply-int 360 x) width)))
+  [^long x ^long y ^long width ^long height]
+  (let [cx (* 4 (/ (- x (/ width 1.5)) width))
+        cy (* 4 (/ (- y (/ height 2)) height))
+        max-iterations 100
+        iter (mandelbrot cx cy max-iterations)]
+    (if (>= iter max-iterations)
+      0x000000
+      (hue-to-rgb (unchecked-add-int 240 (unchecked-divide-int (unchecked-multiply-int iter 360) max-iterations))))))
 
 (defn set-pixel
   [image-data width x y color]
   (aset-int image-data (unchecked-add-int (unchecked-multiply-int y width) x) color))
 
 (defn draw-line
-  [image-data width y]
+  [image-data width height y]
   (doseq [x (range width)]
-    (set-pixel image-data width x y (get-color x y width))))
+    (set-pixel image-data width x y (get-color x y width height))))
 
 (defn draw
   [frame buffered-image width height]
   (let [image-data (-> buffered-image (.getRaster) (.getDataBuffer) (.getData))]
-    (dorun (pmap #(draw-line image-data width %) (shuffle (range height)))))
+    (dorun (pmap #(draw-line image-data width height %) (shuffle (range height)))))
   (.repaint frame))
 
 (defn create-frame
